68d4e33911d0092411b2870c9fc4ca308b8cc546
Vivek Mishra <v.mishra@f5.com>
2024-08-17 17:16:59 +0530

    dockerization of service

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.21.7 AS build-stage
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY *.go ./
+
+RUN CGO_ENABLED=0 GOOS=linux go build -o /api
+
+FROM build-stage AS run-test-stage
+RUN go test -v ./...
+
+FROM scratch as run-release-stage
+WORKDIR /app
+COPY --from=build-stage /api /api
+EXPOSE 8080
+CMD [ "/api" ]
--- config.go
+++ config.go
@@ -18,7 +18,7 @@ var Envs = initConfig()
 
 func initConfig() Config {
 	return Config{
-		Port:       getEnv("PORT", "9000"),
+		Port:       getEnv("PORT", "8080"),
 		DBUser:     getEnv("DB_USER", "root"),
 		DBPassword: getEnv("DB_PASSWORD", "password"),
 		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3300")),
